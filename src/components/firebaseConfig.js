ef96cd423b87ec8321b21a5d50a15bd17b107ed5
koftov <ahronlu@gmail.com>
2020-05-10 20:50:52 +0300

    fetching and displaying data from firestore and adding view link

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,10 +1,9 @@
 export default {
-    apiKey: "AIzaSyBTwlAYnnlfh7kUAEj-tQ2Jr6nUHEHuLqE",
-    authDomain: "vue-koftov.firebaseapp.com",
-    databaseURL: "https://vue-koftov.firebaseio.com",
-    projectId: "vue-koftov",
-    storageBucket: "vue-koftov.appspot.com",
-    messagingSenderId: "253276047767",
-    appId: "1:253276047767:web:842d94f5a3fd832efd1cd2"
-  };
-}
+  apiKey: "AIzaSyBTwlAYnnlfh7kUAEj-tQ2Jr6nUHEHuLqE",
+  authDomain: "vue-koftov.firebaseapp.com",
+  databaseURL: "https://vue-koftov.firebaseio.com",
+  projectId: "vue-koftov",
+  storageBucket: "vue-koftov.appspot.com",
+  messagingSenderId: "253276047767",
+  appId: "1:253276047767:web:842d94f5a3fd832efd1cd2"
+};
--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,5 +1,5 @@
 import firebase from "firebase";
-import firestore from "firebase/firstore";
+import firestore from "firebase/firestore";
 import firebaseConfig from "./firebaseConfig";
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 export default firebaseApp.firestore();
