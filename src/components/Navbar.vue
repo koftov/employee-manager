fd4efa2001f35580982fd8bf8f073d7c35cdcdcf
koftov <ahronlu@gmail.com>
2020-05-10 22:40:09 +0300

    adding authentication with firebase

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,58 @@
   <nav>
     <div class="nav-wrapper green">
       <div class="container">
-        <router-link to="/" class="brand-logo">Employee Manager</router-link>
+        <router-link to="/" class="brand-logo left hide-on-med-and-down">Employee Manager</router-link>
+        <ul class="right">
+          <li v-if="isLoggedIn">
+            <a class="email black-text">{{currentUser}}</a>
+          </li>
+          <li v-if="isLoggedIn">
+            <router-link to="/">Dashboard</router-link>
+          </li>
+          <li v-if="!isLoggedIn">
+            <router-link to="/login">Login</router-link>
+          </li>
+          <li v-if="!isLoggedIn">
+            <router-link to="/register">Register</router-link>
+          </li>
+          <li v-if="isLoggedIn">
+            <a v-on:click="logout">Logout</a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
 </template>
+
+<script>
+import firebase from "firebase";
+export default {
+  name: "navbar",
+  data() {
+    return {
+      isLoggedIn: false,
+      currentUser: false
+    };
+  },
+  created() {
+    if (firebase.auth().currentUser) {
+      this.isLoggedIn = true;
+      this.currentUser = firebase.auth().currentUser.email;
+    }
+  },
+  methods: {
+    logout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => this.$router.go({ path: this.$router.path }));
+    }
+  }
+};
+</script>
+
+<style scoped>
+.email {
+  padding-right: 10px;
+}
+</style>
