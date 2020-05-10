61805ec92dc67da8a86b3b26f5a38b6dc85f5667
koftov <ahronlu@gmail.com>
2020-05-10 20:37:40 +0300

    making the basic shell

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import Dashbord from "@/components/Dashboard";
+import NewEmployee from "@/components/NewEmployee";
+import ViewEmployee from "@/components/ViewEmployee";
+import EditEmployee from "@/components/EditEmployee";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "Dashbord",
+      component: Dashbord
+    },
+    {
+      path: "/new",
+      name: "new-employee",
+      component: NewEmployee
+    },
+    {
+      path: "/edit/:employee_id",
+      name: "edit-employee",
+      component: EditEmployee
+    },
+    {
+      path: "/:employee_id",
+      name: "view-employee",
+      component: ViewEmployee
     }
   ]
-})
+});
